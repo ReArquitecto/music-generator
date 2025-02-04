366e649630cd501c1da4d7ee101460121da90e23
Jeff Learman <jjlearman@gmail.com>
2025-02-04 12:36:57 -0500

    dark mode, refactor display_musicxml

--- src/jtest.py
+++ src/jtest.py
@@ -1,5 +1,7 @@
 from music21 import stream, note, chord, meter
 
+import web
+
 score = stream.Score()
 part = stream.Part()
 part.append(note.Note('C4', quarterLength=1))  # C4, quarter note
@@ -9,22 +11,18 @@ part.append(chord.Chord(['G4', 'B4'], quarterLength=1))  # G4 and B4, quarter ch
 score.append(meter.TimeSignature('4/4'))
 score.append(part)
 
+score.write('musicxml', 'output/jtest.xml')
+
 # Display the score using OpenSheetMusicDisplay
 from IPython.display import display, HTML
 import webbrowser
 import tempfile
 from pathlib import Path
 
-'''Display the score using OpenSheetMusicDisplay'''
-score.write('musicxml', 'output/jtest.xml')
+# write the html file
+web.display_musicxml('output/jtest.html', 'output/jtest.xml')
 
-# TODO: write the html file %%%
-
-if False:
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.html') as tmp_html:
-        tmp_html.write(html.encode('utf-8'))
-        webbrowser.open(tmp_html.name)
-
+# open it in the browser
 # NOTE: before running, open a webserver using `python -m http.server` in this directory
 webbrowser.open('http://localhost:8000/output/jtest.html')
 
